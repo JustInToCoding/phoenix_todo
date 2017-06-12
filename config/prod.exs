191244e0cfe94bae5a7acd688c297fec2dbd33ab
Justin Steenhuis <justinsteenhuis@gmail.com>
2017-06-13 00:29:55 +0200

    configure prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_todo, PhoenixTodo.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "thawing-headland-78405.herokuapp.com ", port: 443],
-  cache_static_manifest: "priv/static/manifest.json",
+  url: [scheme: "https", host: "thawing-headland-78405.herokuapp.com ", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
